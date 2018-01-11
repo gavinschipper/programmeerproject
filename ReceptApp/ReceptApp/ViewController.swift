da351346dd82720d5e0343299e73d850f414dc00
Gavin Schipper <gavin_schipper@hotmail.com>
2018-01-11 22:02:52 +0100

    prototype toegevoegd

---
+++ ReceptApp/ReceptApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReceptApp
+//
+//  Created by Gavin Schipper on 11-01-18.
+//  Copyright © 2018 Gavin Schipper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
