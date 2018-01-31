67766859f9c6e2a7b10eda80b506162ab1ee776b
Gavin Schipper <gavin_schipper@hotmail.com>
2018-01-31 14:13:13 +0100

    createAccountButtonPressed() korter gemaakt

--- Project/ReceptApp/Controllers/CreateAccountViewController.swift
+++ Project/ReceptApp/Controllers/CreateAccountViewController.swift
@@ -35,43 +35,46 @@ class CreateAccountViewController: UIViewController {
             present(alertController, animated: true, completion: nil)
             
         } else {
-            
             let email = self.emailTextField.text!
             let username = self.usernameTextField.text!
             let password = self.passwordTextField.text!
             
-            Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            createUser(username: username, email: email, password: password)
+        }
+    }
+    
+    func createUser(username: String, email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            
+            // Als er geen error is, ga terug naar login pagina
+            if error == nil {
                 
-                // Als er geen error is, ga terug naar login pagina
-                if error == nil {
-                    
-                    guard let uid = user?.uid else {
-                        return
-                    }
-                    
-                    let userReference = self.ref.child("users").child(uid)
-                    let values = ["username": username, "email": email]
-                    
-                    userReference.updateChildValues(values)
-                    
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "startScherm")
-                    self.present(vc!, animated: true, completion: nil)
-                    
+                guard let uid = user?.uid else {
+                    return
                 }
-                    
+                
+                let userReference = self.ref.child("users").child(uid)
+                let values = ["username": username, "email": email]
+                
+                userReference.updateChildValues(values)
+                
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "startScherm")
+                self.present(vc!, animated: true, completion: nil)
+                
+            }
+                
                 // Als er een error is, laat een waarschuwing zien met wat het probleem is.
-                else {
-                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
-                    // zorgt ervoor dat de alert weggeklikt kan worden
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    
-                    self.present(alertController, animated: true, completion: nil)
-                }
+            else {
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                // zorgt ervoor dat de alert weggeklikt kan worden
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
             }
         }
-        
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
